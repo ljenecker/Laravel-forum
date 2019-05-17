0fc3463033a72ed55ef89b907c382f69489d9a3c
Lorenzo <ljenecker@gmail.com>
2019-05-17 23:11:15 +0200

    user_may_publish_threads

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{$thread->title}}</div>
+                <div class="card-header">
+                <a href='#'> {{$thread->creator->name}} </a> posted:
+                {{$thread->title}}</div>
 
                 <div class="card-body">
                     {{$thread->body}}
@@ -13,5 +15,32 @@
             </div>
         </div>
     </div>
+    <hr>
+    <div class="row justify-content-center">
+    @foreach ($thread->replies as $reply)
+        @include ('threads.reply')
+    @endforeach
+    </div>
+
+    @if (auth()->check())
+    <hr>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form method='POST' action="{{ $thread->path() . '/replies' }}">
+
+            @csrf
+
+            <div class="form-group">
+                <textarea class="form-control" name="body" id="body" placeholder="Have something to say?" rows="5"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary mb-2">Post</button>
+
+            </form>
+        </div>
+    </div>
+    @else
+    <p class="text-center">Please <a href="{{route('login')}}">sign in</a> to particioate in this discussion</p>
+    @endif
 </div>
 @endsection
---
+++ tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ParticipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+
+    function unauthenticate_user_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory('App\Thread')->create();
+
+        $reply = factory('App\Reply')->create();
+
+        $this->post('threads/1/replies');
+    }
+
+    /** @test */
+
+    function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        $user = factory('App\User')->create();
+        $this->be($user);
+
+        $thread = factory('App\Thread')->create();
+
+        $reply = factory('App\Reply')->make();
+
+        $this->post($thread->path() . '/replies', $reply->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+
+
+}

193ba98042fd7cf72dff485af7b46b243bee76fd
Lorenzo <ljenecker@gmail.com>
2019-05-18 17:08:02 +0200

    Create tests and controller for creating threads

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    /** @test */
+    public function an_authenticated_user_can_create_new_forum_threads()
+    {
+        // Given that we have a signed in user.
+        $this->actingAs(factory('App\User')->create());
+
+        // When we hit the endpoint to create a new thread.
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+        // Then, when we visit the thread page.
+
+        $response = $this->get($thread->path());
+        // We should see the thread.
+
+        $response->assertSee($thread->title)
+             ->assertSee($thread->body);
+
+    }
+}
